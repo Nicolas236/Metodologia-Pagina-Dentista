4595c1f3bbb45ca516b88617e7c9a85d11ed7240
Jeremias Alvarez <jerealvarez34@gmail.com>
2020-09-21 22:13:40 -0300

    mail

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,4 +1,11 @@
 const form = $(".php-email-form")[0];
+const name = $("#from_name");
+const email = $("#from_email");
+const tel = $("#tel");
+const msg = $("#msg");
+const selectBox = $("#type");
+const btn = $("#submit");
+let img = "";
 
 const errorList = $("#errorList");
 
@@ -6,10 +13,9 @@ function addError(field) {
     errorList.append(`<li>El campo ${field} no es valido</li>`);
 }
 
+$("#ficha").on("change", readFile);
+
 function validate() {
-    const name = $("#from_name");
-    const email = $("#from_email");
-    const tel = $("#tel");
     // const name = $('#from_name');
 
     let check = true;
@@ -51,12 +57,67 @@ function validate() {
     return check;
 }
 
-const btn = $("#submit");
+function getCheckboxes() {
+    let days = [];
+    let hours = [];
+
+    let selectedValues = [];
+
+    $.each($("input[name='day']:checked"), function () {
+        days.push($(this).val());
+    });
+    $.each($("input[name='moment']:checked"), function () {
+        hours.push($(this).val());
+    });
+
+    selectedValues.push(days.join(", "), hours.join(", "));
+
+    return selectedValues;
+}
+
+function readFile() {
+    if (this.files && this.files[0]) {
+        var FR = new FileReader();
+
+        FR.addEventListener("load", function (e) {
+            console.log(e.target.result);
+            img = e.target.result;
+        });
+
+        FR.readAsDataURL(this.files[0]);
+    }
+}
+
+function getDataUri(url, callback) {
+    var image = new Image();
+
+    image.onload = function () {
+        var canvas = document.createElement("canvas");
+        canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+        canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+
+        canvas.getContext("2d").drawImage(this, 0, 0);
+
+        // Get raw image data
+        callback(
+            canvas
+                .toDataURL("image/png")
+                .replace(/^data:image\/(png|jpg);base64,/, "")
+        );
+
+        // ... or get as Data URI
+        callback(canvas.toDataURL("image/png"));
+    };
+
+    image.src = url;
+    img.crossOrigin = "Anonymous";
+}
 
 $("form").on("submit", function (event) {
     event.preventDefault();
 
     errorList.empty();
+    console.log(img);
 
     if (!validate()) {
         return;
@@ -67,8 +128,37 @@ $("form").on("submit", function (event) {
     const serviceID = "default_service";
     const templateID = "template_3sgl7e3";
 
-    emailjs.sendForm(serviceID, templateID, form).then(
-        () => {
+    const selectedValue = selectBox.children("option:selected").val();
+
+    const checkBoxes = getCheckboxes();
+
+    const days = checkBoxes[0];
+    const hours = checkBoxes[1];
+
+    console.log(`${checkBoxes}\n${days}\n${hours}`);
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "lospiratasutn@gmail.com",
+        Password: "656DC89B6C2547A989C6E8377DA12E200DC2",
+        To: "lospiratasutn@gmail.com",
+        From: "lospiratasutn@gmail.com",
+        Subject: "Solicitud de Turno - Consultorio Sonrisa Feliz",
+        Body: `Hola Doctora Sonrisa, tiene una nueva solicitud de turno. <br />
+
+        Especificaciones del paciente: <br />
+
+        Nombre: ${name.val()} <br />
+        Email: ${email.val()} <br />
+        Telefono: ${tel.val()} <br />
+        Tipo de Consulta solicitada: ${selectedValue} <br />
+        Dia(s) preferido(s): ${days} <br />
+        Horario(s) preferido(s): ${hours} <br />
+        Mensaje: ${msg.val()} <br />
+        Ficha Medica: <img src="${img}" alt="ficha"/>
+        `,
+    }).then(
+        (message) => {
             btn.text("Solicitar Turno");
             swal({
                 title: "Envio exitoso!",
@@ -102,4 +192,40 @@ $("form").on("submit", function (event) {
             });
         }
     );
+
+    // emailjs.sendForm(serviceID, templateID, form).then(
+    //     () => {
+    //         btn.text("Solicitar Turno");
+    //         swal({
+    //             title: "Envio exitoso!",
+    //             text:
+    //                 "Tu solicitud se envio correctamente. La doctora se comunicara con usted a la brevedad.",
+    //             icon: "success",
+    //             button: {
+    //                 text: "Entendido",
+    //                 value: true,
+    //                 visible: true,
+    //                 className: "btn btn-primary btn-xl js-scroll-trigger",
+    //                 closeModal: true,
+    //             },
+    //         });
+    //         form.reset();
+    //     },
+    //     (err) => {
+    //         btn.text("Solicitar Turno");
+    //         swal({
+    //             title: "Error",
+    //             text:
+    //                 "Lo sentimos, ocurrio un error y tu solicitud no se envio correctamente",
+    //             icon: "error",
+    //             button: {
+    //                 text: "Entendido",
+    //                 value: true,
+    //                 visible: true,
+    //                 className: "btn btn-primary btn-xl js-scroll-trigger",
+    //                 closeModal: true,
+    //             },
+    //         });
+    //     }
+    // );
 });
